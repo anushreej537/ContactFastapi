68bc501eda9b28addbaccb8d834686ebbd6a8ad2
anushreej537 <anushreej537@gmail.com>
2024-02-01 22:32:55 +0530

    finish

--- user/api.py
+++ user/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .pydantic import Contact_user
+from .pydantic import Contact_user,Del_data,updateuser
 from .models import Contact
 
 app = APIRouter()
@@ -16,3 +16,26 @@ async def contact_detail(data:Contact_user):
         obj = await Contact.create(name = data.name,email = data.email,
                              mobilenum = data.phone, city = data.city)
         return {'obj':obj}
+    
+
+@app.get('/get_all_data')
+async def all_detail():
+    obj = await Contact.all()
+    return (obj)
+
+
+@app.delete('/deldata')
+async def delete_data(data:Del_data):
+    await Contact.get(id=data.id).delete()
+    return {"messsage" : 'user delete successfully'}
+
+
+@app.put("/updatedata")
+async def update_user(data:updateuser):
+    user_obj = await Contact.get(id=data.id)
+    if not user_obj:
+        return {"status":False, "message":"User Not found"}
+    else:
+        await Contact.filter(id=data.id).update(name=data.name,email=data.email,
+                                              mobilenum=data.phone,city=data.city)
+        return {"status":True, "message":"user update sucessfully"}
--- user/pydantic.py
+++ user/pydantic.py
@@ -5,3 +5,14 @@ class Contact_user(BaseModel):
     email: str
     phone:int
     city:str
+
+class Del_data(BaseModel):
+    id:int
+
+
+class updateuser(BaseModel):
+    id:int
+    name:str
+    email:str
+    phone:int
+    city:str
